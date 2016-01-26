32db76d240d98557cafd700705a57f9fa2865d06
WorkPashtet <work.pashtet@gmail.com>
2016-01-26 15:32:53 -0500

    huge clean up

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./src/Directives/FileDropZone";
-export * from "./src/Directives/FileDroppa";
-export * from "./src/Directives/FileList";
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Directives/FileDropZone";
-export * from "./Directives/FileDroppa";
-export * from "./Directives/FileList";
